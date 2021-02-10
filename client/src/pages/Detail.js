ed71d5e583cc779c2c1b159d92a7324057aa0905
Mai Yia Xiong <maiyiax@gmail.com>
2021-02-10 15:49:06 -0600

    added global components

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+
+import { QUERY_PRODUCTS } from "../utils/queries";
+import spinner from '../assets/spinner.gif'
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
+
+function Detail() {
+  const [state, dispatch] = useStoreContext();
+  const { id } = useParams();
+
+  const [currentProduct, setCurrentProduct] = useState({})
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const { products } = state;
+
+  // checks if there's data in gobal state's product array and use it to figure out current product to display otherwise use product data returned from useQuery and set that data to the global state object
+  useEffect(() => {
+    if (products.length) {
+      setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+    }
+  }, [products, data, dispatch, id]); // dependency array: hook's functionality only runs when it detects changes in values from the array
+
+  return (
+    <>
+      {currentProduct ? (
+        <div className="container my-1">
+          <Link to="/">
+            ← Back to Products
+          </Link>
+
+          <h2>{currentProduct.name}</h2>
+
+          <p>
+            {currentProduct.description}
+          </p>
+
+          <p>
+            <strong>Price:</strong>
+            ${currentProduct.price}
+            {" "}
+            <button>
+              Add to Cart
+            </button>
+            <button>
+              Remove from Cart
+            </button>
+          </p>
+
+          <img
+            src={`/images/${currentProduct.image}`}
+            alt={currentProduct.name}
+          />
+        </div>
+      ) : null}
+      {
+        loading ? <img src={spinner} alt="loading" /> : null
+      }
+    </>
+  );
+};
+
+export default Detail;
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+// instatiate the global state object
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    // returns most up to date version of our global state object and the method executed to update the state
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
